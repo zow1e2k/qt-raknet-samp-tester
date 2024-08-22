82050ce33aa47fbe60658729ba9292adc87149b7
Артем Бахмутенко <kadomodify@gmail.com>
2024-08-22 16:05:37 +0400

    feat: implementing RakNet

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include "RakNet/src/RakPeerInterface.h"
+#include <stdio.h>
 
 #define MAX_CLIENTS 10
 #define SERVER_PORT 60000
 
 int main(int argc, char *argv[]) {
+    RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
+
+    printf("(C) or (S)erver?\n");
+    RakNet::SocketDescriptor sd;
+    peer->Startup(1,&sd, 1);
+
+    RakNet::RakPeerInterface::DestroyInstance(peer);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
